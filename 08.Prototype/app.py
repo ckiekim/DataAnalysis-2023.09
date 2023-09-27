f84f04c4bba5c9fb05021fadcd7beb74b3acb8aa
Cheolkie Kim <ckiekim@naver.com>
2023-09-27 10:51:09 +0900

    Prototype

--- 08.Prototype/app.py
+++ 08.Prototype/app.py
@@ -1,23 +1,22 @@
-from flask import Flask, render_template, request
-import util.crawl_util as cu
+from flask import Flask, render_template, request, flash
+from bp.crawling import crawl_bp
+from bp.map import map_bp
 
 app = Flask(__name__)
+app.secret_key = 'qwert12345'       # flash와 session을 사용하려면 반드시 설정해야 함
+
+app.register_blueprint(crawl_bp, url_prefix='/crawling')    # localhost:5000/crawling/* 는 crawl bp가 처리
+app.register_blueprint(map_bp, url_prefix='/map')
 
 @app.route('/')
 def home():
-    menu = {'ho':1, 'us':0, 'cr':0, 'sc':0}
+    menu = {'ho':1, 'us':0, 'cr':0, 'ma':0, 'sc':0}
+    flash('Welcome to my Web!!!')
     return render_template('home.html', menu=menu)
-
-@app.route('/melon')
-def melon():
-    menu = {'ho':0, 'us':0, 'cr':1, 'sc':0}
-    song_list = cu.get_melon_chart()
-    return render_template('melon.html', song_list=song_list, menu=menu)
-
 
 @app.route('/schedule')
 def schedule():
-    menu = {'ho':0, 'us':0, 'cr':0, 'sc':1}
+    menu = {'ho':0, 'us':0, 'cr':0, 'ma':0, 'sc':1}
     return render_template('schedule.html', menu=menu)
 
 if __name__ == '__main__':
---
+++ 08.Prototype/bp/crawling.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, current_app
+import util.crawl_util as cu
+
+crawl_bp = Blueprint('crawl_bp', __name__)
+
+menu = {'ho':0, 'us':0, 'cr':1, 'ma':0, 'sc':0}
+
+@crawl_bp.route('/melon')
+def melon():
+    print(current_app.root_path)    # bp module에서는 app 대신에 current_app을 사용
+    song_list = cu.get_melon_chart()
+    return render_template('crawling/melon.html', song_list=song_list, menu=menu)
+
+@crawl_bp.route('/interpark')
+def interpark():
+    book_list = cu.get_bestseller()
+    return render_template('crawling/interpark.html', book_list=book_list, menu=menu)

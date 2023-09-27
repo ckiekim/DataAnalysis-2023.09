96a8976b6fa5fa4b44766dca26b604f5d68856c8
Cheolkie Kim <ckiekim@naver.com>
2023-09-27 12:18:53 +0900

    Prototype

--- 08.Prototype/app.py
+++ 08.Prototype/app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 from bp.crawling import crawl_bp
 from bp.map import map_bp
+from bp.user import user_bp
+import os, random
 
 app = Flask(__name__)
 app.secret_key = 'qwert12345'       # flash와 session을 사용하려면 반드시 설정해야 함
+app.config['SESSION_COOKIE_PATH'] = '/'
 
 app.register_blueprint(crawl_bp, url_prefix='/crawling')    # localhost:5000/crawling/* 는 crawl bp가 처리
 app.register_blueprint(map_bp, url_prefix='/map')
+app.register_blueprint(user_bp, url_prefix='/user')
+
+@app.before_first_request
+def before_first_request():         # 최초 1회 실행
+    global quotes, quote
+    global addr
+    filename = os.path.join(app.static_folder, 'data/todayQuote.txt')
+    with open(filename, encoding='utf-8') as file:
+        quotes = file.readlines()
+    quote = random.sample(quotes, 1)[0]
+    session['quote'] = quote
+    addr = '서울시 영등포구'
+    session['addr'] = addr
 
 @app.route('/')
 def home():
     menu = {'ho':1, 'us':0, 'cr':0, 'ma':0, 'sc':0}
-    flash('Welcome to my Web!!!')
+    # flash('Welcome to my Web!!!')
     return render_template('home.html', menu=menu)
 
 @app.route('/schedule')
